d8133b8bc3563111731b9c75307148ad9f7ad238
neener <janinemrosen@gmail.com>
2016-12-11 20:01:51 -0500

    defining post route to uploads to handle file submission

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,2 +1,15 @@
 class UploadsController < ApplicationController
+	post '/upload' do
+		# Check if use uploaded a file
+		if params[:image] && params[:image][:filename]
+			filename = params[:image][:filename]
+			file = params[:image][:tempfile]
+			path = "./public/uploads/#{filename}"
+
+			# Write file to disk
+			File.open(path, 'wb') do |f|
+				f.write(file.read)
+			end
+		end
+	end
 end
